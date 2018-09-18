b870c9965a6a0cccecd6d8b2657fb40dccdd793c
Romain Lefort <rlefort@beapi.fr>
2018-09-18 15:19:30 +0200

    Clean extension file

--- src/extension.ts
+++ src/extension.ts
@@ -6,18 +6,17 @@ const ncp = require('copy-paste');
 
 export function activate(context: vscode.ExtensionContext) {
 	// Following are just data provider samples
-	const sassFileRoute = vscode.workspace.getConfiguration('sassVariablesHelper').route
-	console.log(vscode.workspace.getConfiguration('sassVariablesHelper'));
-	let rootPath = ''
+	const sassFileRoute = vscode.workspace.getConfiguration('sassVariablesHelper').route;
+	let rootPath = '';
 	if (sassFileRoute) {
-		rootPath = vscode.workspace.rootPath + sassFileRoute
+		rootPath = vscode.workspace.rootPath + sassFileRoute;
 	}
 	const sassVariablesProvider = new colorProvider(rootPath);
 
 	vscode.window.registerTreeDataProvider('sassVariables', sassVariablesProvider);
 	vscode.commands.registerCommand('sassVariables.refreshEntry', () => sassVariablesProvider.refresh());
 	vscode.commands.registerCommand('extension.openPackageOnNpm', colorName => {
-		ncp.copy(`$${colorName}`, function () {
+		ncp.copy(`$${colorName}`, () => {
 			vscode.window.showInformationMessage('Variable copied to clipboard');
 		})
 	});

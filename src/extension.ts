c73863f1e8a6e7fe60e178cfc1d56148c60ab6f3
Romain Lefort <rlefort@beapi.fr>
2018-09-27 18:30:44 +0200

    #2 - prepend slash to file vars route

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ const ncp = require('copy-paste');
 
 export function activate(context: vscode.ExtensionContext) {
 	// Following are just data provider samples
-	const sassFileRoute = vscode.workspace.getConfiguration('sassVariablesHelper').route;
+	const sassFileRoute = vscode.workspace.getConfiguration('sassVariablesHelper').route[0] === '/' ? vscode.workspace.getConfiguration('sassVariablesHelper').route : `/${vscode.workspace.getConfiguration('sassVariablesHelper').route}`
 	let rootPath = '';
 	if (sassFileRoute) {
 		rootPath = vscode.workspace.rootPath + sassFileRoute;

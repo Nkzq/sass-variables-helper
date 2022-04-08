f8710c2bc83b6e2902125f9f26181639ab2fad92
Romain Lefort <romain.lefort@goandup.fr>
2022-04-08 17:36:48 +0200

    fix(refresh)

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,25 @@
-'use strict';
-
 import * as vscode from 'vscode';
-import { colorProvider } from './sassVariables'
-const ncp = require('@nkzq/copy-paste')
+import { colorProvider } from './sassVariables';
+const ncp = require('@nkzq/copy-paste');
 
 export function activate(context: vscode.ExtensionContext) {
-	// Following are just data provider samples
+
 	const sassFileRoute = vscode.workspace.getConfiguration('sassVariablesHelper').route[0] === '/' ? vscode.workspace.getConfiguration('sassVariablesHelper').route : `/${vscode.workspace.getConfiguration('sassVariablesHelper').route}`
 	let rootPath = sassFileRoute;
 
 	if (sassFileRoute && vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length < 2) {
 		rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath + sassFileRoute;
 	}
+
 	const sassVariablesProvider = new colorProvider(rootPath);
 
 	vscode.window.registerTreeDataProvider('sassVariables', sassVariablesProvider);
 
-	context.subscriptions.push(vscode.commands.registerCommand('sassVariables.refreshEntry', () => sassVariablesProvider.refresh()));
+	const disposable = vscode.commands.registerCommand('sassVariables.refresh', () => {
+		sassVariablesProvider.refresh();
+	});
+
+	context.subscriptions.push(disposable);
 
 	vscode.commands.registerCommand('extension.openPackageOnNpm', colorName => {
 		ncp.copy(`$${colorName}`, () => {
